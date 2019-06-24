1dfce6ebeff34b39d9045a8a5fba7dcb47328746
Ogek <ogek@develer.com>
2019-06-24 09:52:14 +0200

    Entry point

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py', silent=True)
+    
+    from api import bp
+    app.register_blueprint(bp, url_prefix='/api')
+
+    from database import db
+    db.init_app(app)
+
+    return app
