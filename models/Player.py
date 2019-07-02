9f04612ab16b6d5b8c62b58415d7e958d438c55a
Ogek <ogek@develer.com>
2019-07-02 14:10:12 +0200

    Added slack fields not mapped

--- models/Player.py
+++ models/Player.py
@@ -1,3 +1,4 @@
+from marshmallow import fields, validate
 from flask_marshmallow import Marshmallow
 
 from database import db
@@ -8,11 +9,26 @@ ma = Marshmallow()
 class Player(db.Model):
     __tablename__ = 'players'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    nickname = db.Column(db.String(30), unique=True, nullable=False)
-    nDisonors = db.Column('n_disonors', db.Integer, nullable=False, default=0)
+    slack_id = db.Column(db.String, nullable=False, unique=True)
+    dishonors = db.Column(db.Integer,
+                          nullable=False,
+                          default=0,
+                          server_default="0")
+    nickname = None
+    name = None
+
+    def merge_slack_user(self, slack_user: dict):
+        self.nickname = slack_user.get('real_name', "")
+        self.name = slack_user.get('display_name', "")
 
 
 class PlayerSchema(ma.ModelSchema):
     class Meta:
         model = Player
+
+    nickname = fields.String(required=True,
+                             validate=validate.Length(1),
+                             dump_only=True)
+    name = fields.String(required=True,
+                         validate=validate.Length(1),
+                         dump_only=True)
