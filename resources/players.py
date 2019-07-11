f5cfbbd5c5763d125e701da171dea9216957b541
Ogek <ogek@develer.com>
2019-07-11 12:16:02 +0200

    Fixed search paramenter

--- resources/players.py
+++ resources/players.py
@@ -18,7 +18,7 @@ class PlayersResource(Resource):
         slack = SlackHelper(current_app.config['SLACK_TOKEN'])
         players = Player.query.all()
         try:
-            slack_users = slack.get_users(search=request.args.get("s"))
+            slack_users = slack.get_users(searchTerm=request.args.get("s"))
         except SlackRequestFailed as e:
             logging.error('Slack Api Error: {}'.format(str(e)))
             return Response.error({'general': [Response.REQUEST_FAILED]}, 503)
