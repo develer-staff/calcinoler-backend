73d5b6d9aebc6a2ef510fb45a612e6e910a2ce41
Ogek <ogek@develer.com>
2019-07-11 16:48:44 +0200

    Added return type and description in docstring for 'validate_schema_unknown_fields' function

--- utils/schema.py
+++ utils/schema.py
@@ -1,13 +1,17 @@
 from flask_marshmallow.schema import Schema
 
 
-def validate_schema_unknown_fields(schema: Schema, data: dict):
-    """ Validates if in data there are fields not present
-        or dump_only=True in Schema
+def validate_schema_unknown_fields(schema: Schema, data: dict) -> dict:
+    """Validates if in data there are fields not present
+        or dump_only=True in Schema.
 
-        - schema (flask_marshmallow.schema.Schema): Instance of Schema
-        - data (dict): Data to validate
+        - schema (flask_marshmallow.schema.Schema):
+            Instance of Schema.
+        - data (dict):
+            Data to validate.
 
+        Returns errors in form of dict. As key the field and
+        as value a list of errors.
     """
     errors = {}
     for field in data:
